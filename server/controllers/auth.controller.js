8cb594b2b3101d667e070345975d9bd5c6596085
bhadaliyasahaj <131737975+bhadaliyasahaj@users.noreply.github.com>
2024-08-10 20:39:24 +0530

    Update auth.controller.js

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -34,7 +34,8 @@ export const signin = async (req,res,next)=>{
 
         res.cookie("access_token", token,{
             httpOnly:true,
-            maxAge: 365 * 24 * 60 * 60 * 1000
+            maxAge: 365 * 24 * 60 * 60 * 1000,
+            sameSite:"None"
         })
         .status(200)
         .json(others)
