d2a67405ee09c35dbc3a12ef77ee43a18116f295
bhadaliyasahaj <bhadaliyasahaj@gmail.com>
2024-08-11 08:20:16 +0530

    cors

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -35,7 +35,8 @@ export const signin = async (req,res,next)=>{
         res.cookie("access_token", token,{
             httpOnly:true,
             maxAge: 365 * 24 * 60 * 60 * 1000,
-            secure:false
+            secure:false,
+            sameSite:'None'
            })
         .status(200)
         .json(others)
