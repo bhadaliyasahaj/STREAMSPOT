02ec7ce3f2b83cd010878160fa7be656780a9f0b
bhadaliyasahaj <bhadaliyasahaj@gmail.com>
2024-08-31 09:00:05 +0530

    remove built from server

--- server/index.js
+++ server/index.js
@@ -16,16 +16,10 @@ dotenv.config();
 
 const app = express()
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-app.use(express.static(path.join(__dirname, 'build')));
-
-
 const corsOptions = {
-    origin:process.env.CLIENT_URL, // Set your frontend URL
-    credentials: true, // Allow credentials (cookies, etc.) to be sent
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+    origin:process.env.CLIENT_URL, 
+    credentials: true, 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
 };
 
 
@@ -33,11 +27,6 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(cors(corsOptions))
 
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL);
-//     res.header("Access-Control-Allow-Credentials", "true");
-//     next();
-// });
 
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
@@ -56,9 +45,6 @@ app.use((err,req,res,next)=>{
     })
 })
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
 app.listen(process.env.PORT,()=>{
     connectDB();
